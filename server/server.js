6325d2992b78380ab950a646e3dcb9faf726574e
LeninMiranda <lenin9073@gmail.com>
2025-06-19 16:17:48 -0700

    Cors Options

--- server/server.js
+++ server/server.js
@@ -8,7 +8,14 @@ const { PORT = 3001 } = process.env;
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: "https://lenin-miranda.github.io",
+  methods: "GET,POST,PUT,DELETE,OPTIONS",
+  allowedHeaders: "Content-Type,Authorization",
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use("/send-email", emailRouter);
 
